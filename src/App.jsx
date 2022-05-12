243dc1462f935861a020dd044e4561ea46f908c4
kota-github-hirano <97215283+kota-github-hirano@users.noreply.github.com>
2022-05-12 16:36:39 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div></div>
+      <div></div>
+    </>
+  );
+};

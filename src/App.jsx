07841d17dd639b95eb4ebf09916508bbef1db85e
kota-github-hirano <97215283+kota-github-hirano@users.noreply.github.com>
2022-05-17 15:29:51 +0900

    cssの設定 jsのcss設定を持ってきて紐付け

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,40 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
+        {/* Reactの場合はclassではなくclassName */}
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div></div>
-      <div></div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="list-row">
+            <li>いいいいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ううううう</li>
+            <button>戻す</button>
+          </div>
+          <div className="list-row">
+            <li>えええええ</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
     </>
   );
 };
